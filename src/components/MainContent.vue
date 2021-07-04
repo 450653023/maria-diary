f3981656004309d87160e6a22e4674f5dde9b434
quanmeiling <quanmengling@jwzg.com>
2021-07-04 17:52:29 +0800

    content

--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -4,18 +4,13 @@
       <img class="top-video-img" src="../assets/img/banner.jpg" />
     </div>
     <div class="main_bd">
-      <p class="title">原来产后抑郁症和产后情绪低落的区别在这里！</p>
-      <p class="author">Healthwise 美国著名的医疗健康信息服务商</p>
-      <p class="time">更新于 2018-06-19</p>
+      <p class="title">小玲玲的观音生活</p>
+      <p class="author">小玲玲：伟大的摸鱼家、世界级FE工程师、小姐姐杀手</p>
+      <p class="time">更新于 2021-07-04</p>
     </div>
     <div class="main_ft">
-      <ModTitle msg="视频简介" />
-      <p class="content">抑郁症是女性最高发的心理疾病。目前全球范围内每年新发乳腺癌达 168
-        万人，每年有 52
-        万女性因乳腺癌而死亡。科学地进行乳房检查有利于尽早发现乳腺疾病现乳腺抑郁症是女性最高发的心理疾病。目前全球范围内每年新发乳腺癌达
-        168 万人，每年有 52
-        万女性因乳腺癌而死亡。科学地进行乳房检查有利于尽早发现乳腺疾病现乳腺
-      </p>
+      <ModTitle msg="作者简介" />
+      <p class="content">00后十大优秀小姐姐，擅于整理、写作、耍酷、口嗨、meme制作。喜欢吃价格稍微昂贵的甜食、包装良好的咖啡、重口味烧烤、火锅。</p>
     </div>
   </div>
 </template>
